9360fc6d955d5f87aab854ef9eb0c2b17ca395c7
Vincent Mézino <vincent.mezino@gmail.com>
2018-05-28 00:52:58 +0200

    Fix call to API

--- my-app/src/app/line.service.ts
+++ my-app/src/app/line.service.ts
@@ -5,9 +5,15 @@ import { Http, Response, Headers, RequestOptions } from "@angular/http";
   providedIn: "root"
 })
 export class LineService {
+  private _lines = []
   constructor(private _http: Http) {
-    this._http.get("https://api-ratp.pierre-grimaud.fr/v3").subscribe(data => {
+    this._http.get("https://api-ratp.pierre-grimaud.fr/v3/lines").subscribe(data => {
       let json_data = data.json();
+      this._lines = json_data.result.metros;
     });
+  }
+
+  getLines() {
+    return this._lines;
   }
 }
